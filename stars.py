ea5933c9f844e82e622c6e7bb9c8bd018b53ed03
Daniel <clancy.daniel@gmail.com>
2018-02-15 17:33:22 -0600

    first commit

---
+++ stars.py
@@ -0,0 +1,23 @@
+def stars(arr):
+	for x in arr:
+		output = ""
+		if type(x) is int:
+			for i in range(x):
+				output += "*"
+			print output
+		elif type(x) is str:
+			for i in range(len(x)):
+				output += x[0].lower()
+			print output
+
+x = [4,6,1,3,5,7,25]
+
+stars(x)
+
+print ""
+print ""
+print ""
+
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+
+stars(x)
